1dda950ba8f1b7973a9f98081afe16391ef0d6b8
Sam Jonston <johnstonsam712@gmail.com>
2022-12-06 23:13:45 +0000

    Basic styling to rsvp form

--- app/components/Itenerary.tsx
+++ app/components/Itenerary.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 
 export default function Itenerary() {
   return (
-    <section>
-      <h1 id="itenerary">Itenerary</h1>
+    <section className='itenerary'>
+      <h1>Itenerary</h1>
       <p>2pm ceremony</p>
     </section>
-  )
+  );
 }
